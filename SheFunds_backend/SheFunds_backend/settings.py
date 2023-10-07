86b846c471ea29a318394e4ecf3b99f3385cb13b
Julie E Powell <JEPowell@proton.me>
2023-10-08 09:19:25 +1000

    added user config and model

--- SheFunds_backend/SheFunds_backend/settings.py
+++ SheFunds_backend/SheFunds_backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'program.apps.ProgramConfig',
+    'users.apps.UsersConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
