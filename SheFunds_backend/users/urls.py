dda3599701e13c377902feb4d9cb12886dca5b36
Julie E Powell <JEPowell@proton.me>
2023-10-08 18:10:15 +1000

    removed s from user in urls

--- SheFunds_backend/users/urls.py
+++ SheFunds_backend/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users', views.CustomUserList.as_view()),
-    path('users/<int:pk>', views.CustomUserDetail.as_view()),
+    path('user', views.CustomUserList.as_view()),
+    path('user/<int:pk>', views.CustomUserDetail.as_view()),
 ]
