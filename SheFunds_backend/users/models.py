02185880abdd0489001a3038b7a5f2bdc89a1bb3
Julie E Powell <JEPowell@proton.me>
2023-10-09 12:23:30 +1000

    undid change to models

--- SheFunds_backend/users/models.py
+++ SheFunds_backend/users/models.py
@@ -1,20 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
+
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    
-    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-    def create_auth_token(sender, instance=None, created=False, **kwargs):
-        if created:
-            Token.objects.create(user=instance)
 
-    image = models.URLField(max_length=200, blank=True),
+    image = models.URLField(max_length=200, blank=True)
 
     def __str__(self):
         return self.username
