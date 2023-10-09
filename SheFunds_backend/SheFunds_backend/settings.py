9c9282460889faa2b234c0a731fffdb8a9cc9b58
Julie E Powell <JEPowell@proton.me>
2023-10-09 16:12:17 +1000

    settings revert

--- SheFunds_backend/SheFunds_backend/settings.py
+++ SheFunds_backend/SheFunds_backend/settings.py
@@ -141,8 +141,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication'
-    ]
-}
