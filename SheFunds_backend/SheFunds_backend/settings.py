b0544a570ebcf099482f699432b1d8e1f41b35d5
Tracey Nguyen <trace_n@hotmail.com>
2023-10-11 21:22:57 +1000

    changes to sendmail for settings/views

--- SheFunds_backend/SheFunds_backend/settings.py
+++ SheFunds_backend/SheFunds_backend/settings.py
@@ -142,3 +142,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get(
+    'DJANGO_EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get(
+    'DJANGO_EMAIL_HOST_PASSWORD')  # this is actually the app password eg not account pwd 
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
