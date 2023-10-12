4ca844f3c66be1ba938c7453f208685eda01df30
cinzialoi <cinzia.loi@hotmail.com>
2023-10-12 19:20:01 +1000

    Fixed custom permission IsApplicant to allow for GET method only when authenticated

--- SheFunds_backend/program/permissions.py
+++ SheFunds_backend/program/permissions.py
@@ -4,4 +4,6 @@ class IsApplicant(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method == 'POST': # allow POST applicant for anyone
             return True
+        if request.method == 'GET' and request.user.is_authenticated:
+            return True
         return False
